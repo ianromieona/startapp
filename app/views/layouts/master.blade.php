3113dd4fcf867b116271da662050fb44523b26ef
ianromieona <ianromie@gmail.com>
2014-08-16 17:07:44 +0800

    grid assets

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,9 +4,13 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 	{{ HTML::style('assets/css/bootstrap.css'); }}
+	{{ HTML::script('assets/js/demo.css'); }}
+	{{ HTML::script('assets/js/component.css'); }}
 	{{ HTML::style('assets/css/main.css'); }}
 	{{ HTML::script('assets/js/jquery.js'); }}
 	{{ HTML::script('assets/js/bootstrap.min.js'); }}
+	{{ HTML::script('assets/js/classie.js'); }}
+	{{ HTML::script('assets/js/masonry.pkgd.min.js'); }}
 </head>
 <body>
 	@include('layouts.nav')
